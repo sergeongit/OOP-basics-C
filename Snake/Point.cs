14a2ad113dcfbb68d16ce54b19fa1e6c35c0dd8a
chaplygineverad <s.chaplygin@everad.com>
2020-01-15 17:19:11 +0200

    removed method from Draw() to class Point

---
+++ Snake/Point.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Admin
+ * Date: 15.01.2020
+ * Time: 16:35
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Snake
+{
+	public class Point
+	{
+		public int x;
+		public int y;
+		public char sym;
+		
+		public void Draw()
+		{
+			Console.SetCursorPosition(x, y);
+			Console.Write( sym );
+		}
+	}
+}
---
+++ Snake/Program.cs
@@ -0,0 +1,77 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Admin
+ * Date: 15.01.2020
+ * Time: 14:34
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ * 
+ * using System;
+using System.Collections.Generic;
+using.System.Linq;
+using.System.Text;
+using.System.Threading.Tasks;
+
+namespace Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        	Console.WriteLine("Hello mzf")
+        	Console.ReadLine();
+        }
+    }
+}
+
+ * 
+ * 
+ * 
+ * 
+ */
+ 
+using System;
+
+
+namespace Snake
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Point p1 = new Point();
+			p1.x = 1;
+			p1.y = 3;
+			p1.sym = '*';
+			p1.Draw();
+			
+			Point p2 = new Point();
+			p2.x = 4;
+			p2.y = 5;
+			p2.sym = '#';
+			p2.Draw();
+			
+//			int x1 = 1;
+//			int y1 = 3;
+//			char sym1 = '*';
+			
+//			Console.SetCursorPosition(x1, y1);
+//			Console.Write(sym1);
+			
+//			int x2 = 4;
+//			int y2 = 5;
+//			char sym2 = '#';
+//			
+//			Console.SetCursorPosition(x2, y2);
+//			Console.Write(sym2);
+//			
+			Console.ReadLine();
+		}
+		
+		static void Draw(int x, int y, char sym)
+		{
+			Console.SetCursorPosition(x, y);
+			Console.Write( sym );
+		}
+	}
+}
