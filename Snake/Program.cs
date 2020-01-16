3a83bb7a3ac789c2beac119650fe88fd03465833
chaplygineverad <s.chaplygin@everad.com>
2020-01-16 13:54:32 +0200

    learned class List in C#

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -12,7 +12,10 @@
  */
  
 using System;
-
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Snake
 {
@@ -26,13 +29,28 @@ namespace Snake
 			Point p2 = new Point(4,5,'#');
 			p2.Draw();
 			
+			List<int> numList = new List<int>();
+			numList.Add( 0 );
+			numList.Add( 1 );
+			numList.Add( 2 );
+				
+			int x = numList[ 0 ];
+			int y = numList[ 1 ];
+			int z = numList[ 2 ];
+			
+			foreach( int i in numList)
+			{
+				Console.WriteLine( i );
+			}
+			// remove element '0'
+			numList.RemoveAt( 0 );
+			
+			List<Point> pList = new List<Point>();
+			pList.Add( p1 );
+			pList.Add( p2 );
+			
 			Console.ReadLine();
 		}
 		
-		static void Draw(int x, int y, char sym)
-		{
-			Console.SetCursorPosition(x, y);
-			Console.Write( sym );
-		}
 	}
 }
