85fc9b95dd4b663503e643284e1ad7b5327260df
chaplygineverad <s.chaplygin@everad.com>
2020-01-17 12:12:41 +0200

    added Move() for snake

---
+++ Snake/Direction.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Admin
+ * Date: 17.01.2020
+ * Time: 10:05
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Snake
+{
+	/// <summary>
+	/// Description of Direction.
+	/// </summary>
+	public class Direction
+	{
+		public Direction()
+		{
+		}
+	}
+}
